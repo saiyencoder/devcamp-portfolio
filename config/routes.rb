61999ca930285fc6b152fa16da7a1458838e0eca
Jesus Santizo <jsantizo@selectrehab.com>
2019-01-24 20:38:10 -0600

    Configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
 
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  get 'about',   to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
   resources :blogs
   resources :portfolios
-  
+
 end

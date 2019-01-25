f1df966ba72e40e0ed334deef917ed58d5d2d5c3
Jesus Santizo <jsantizo@selectrehab.com>
2019-01-24 21:16:53 -0600

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  get 'about',   to: 'pages#about'
-  get 'contact', to: 'pages#contact'
+  get 'about',         to: 'pages#about'
+  get 'contact',       to: 'pages#contact'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   resources :blogs
-  resources :portfolios
+  resources :portfolios, except: [:show]
 
 end

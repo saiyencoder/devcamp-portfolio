a2a3f876e20efa019a2acea9d8edd1bd95c7ba55
Jesus Santizo <jsantizo@selectrehab.com>
2019-01-29 16:24:45 -0600

    Implemented custom action for updating status

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   get 'contact',       to: 'pages#contact'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   resources :portfolios, except: [:show]
 
 end

47c1c0c056d3d9650bcacc39e9a6c81a58e3ea81
Jesus Santizo <jsantizo@selectrehab.com>
2019-01-31 14:15:00 -0600

    Integrated technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,15 +1,19 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
   # When Portfolio gets initialized, New Method, it will run the set_defaults method
   after_initialize :set_defaults
+
   # Call Placeholder module from concerns folder
   include Placeholder
 
+  has_many :technologies
+
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails") }
+  
   def self.angular
     where(subtitle: "Angular")
   end
-
-  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails") }
 
   def set_defaults
     self.main_image  ||= Placeholder.image_generator(height: "600", width: "400")

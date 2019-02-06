b53e79c41382591623b99b7150353b3be530c041
Jesus Santizo <jsantizo@selectrehab.com>
2019-02-06 12:44:50 -0600

    Customized registration, login, and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # Instead of localhost/users/sign_up -> it will be localhost/register
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   root to: 'pages#home'
 
   get 'about',         to: 'pages#about'

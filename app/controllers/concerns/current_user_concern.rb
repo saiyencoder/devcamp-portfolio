f21c87fce80dd7b0d79724eefec4f0d6792706b2
Jesus Santizo <jsantizo@selectrehab.com>
2019-02-23 16:07:16 -0600

    Implemented authorization engine for portfolio items and blog

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -10,11 +10,20 @@ module CurrentUserConcern
   end
 
   def guest_user
-    OpenStruct.new(name: "Guest User", 
-                   first_name: "Guest", 
-                   last_name: "User", 
-                   email: "guest@example.com"
-                   )
+    # Due to the petergate gem, OpenStruct is not able to use their methods.
+    # OpenStruct.new(name: "Guest User", 
+    #                first_name: "Guest", 
+    #                last_name: "User", 
+    #                email: "guest@example.com"
+    #                )
+    # Luckily we can just create a GuestUser model that inherits from the User model.
+    # Now we can create a guest_user
+    guest = GuestUser.new
+    guest.name = "Guest User"
+    guest.first_name = "Guest"
+    guest.last_name = "User"
+    guest.email = "guest@example.com"
+    guest
   end
 
 end

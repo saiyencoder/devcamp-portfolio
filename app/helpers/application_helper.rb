6031f412081aac1f0e409ca48c49e27a7c4144c8
Jesus Santizo <jsantizo@selectrehab.com>
2019-02-23 22:20:47 -0600

    Updated master layout styles alomg with helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,13 +9,13 @@ module ApplicationHelper
   #   content_tag(:p, "My helper", class: "I am the superior class")
   # end
 
-  def login_helper
+  def login_helper style
     if current_user.is_a?(GuestUser) 
-      (link_to "Register", new_user_registration_path) +
-      "<br>".html_safe +
-      (link_to "Login", new_user_session_path )
+      (link_to "Register", new_user_registration_path, class: style) +
+      " ".html_safe +
+      (link_to "Login", new_user_session_path, class: style )
     else 
-      link_to "Logout", destroy_user_session_path, method: :delete 
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style 
     end
   end
 

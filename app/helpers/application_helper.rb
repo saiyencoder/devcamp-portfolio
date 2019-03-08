834e51470324bdc04c697c6fa9699c38b6be024a
Jesus Santizo <jsantizo@selectrehab.com>
2019-03-08 16:21:01 -0600

    Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,8 +35,53 @@ module ApplicationHelper
     BarneysViewTool::Renderer.copyright 'Barney Stinson', 'Challenge Accepted!'
   end
 
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blogs'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolios'
+      },
+    ]
+  end
+
+  def nav_helper style, tag_type
+  nav_links = ' '
+  
+  nav_items.each do |item|
+  nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+  end
+
+  nav_links.html_safe
+  end
+
+  # Return "active" if the current page (Rails method) matches
+  # whatever "path" we passed in
+  def active? path
+    "active" if current_page? path
+  end
+
 end
 
+# Here Doc:
+# It is used for multi-line strings. It starts with <<WHATEVERNAMEIANT
+# I can type all the strings I want and make sure I close it out with
+# WHATEVERNAMEIANT at the end
 
 # Rule of thumb on when to use html_safe:
 # If you are going to deal with user input,

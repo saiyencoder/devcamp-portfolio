2083956813d3a75cfd8663d62275b098f49364d5
Jesus Santizo <jsantizo@selectrehab.com>
2019-03-10 13:45:22 -0500

    Updated file upload forms and refactored placeholder functionality into view

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,11 +1,13 @@
 class Portfolio < ApplicationRecord
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
 
   # When Portfolio gets initialized, New Method, it will run the set_defaults method
-  after_initialize :set_defaults
+  # Due to the carrierwave gem, the following is overwritten
+  # after_initialize :set_defaults
 
   # Call Placeholder module from concerns folder
-  include Placeholder
+  # Due to the carrierwave gem, the following is overwritten
+  # include Placeholder
 
   # PortfolioUploader is from the carrier gem for uploading images
   # mount_uploader tells portfolio to call carrierwave, the uploader is going
@@ -27,10 +29,11 @@ class Portfolio < ApplicationRecord
     where(subtitle: "Angular")
   end
 
-  def set_defaults
-    self.main_image  ||= Placeholder.image_generator(height: "600", width: "400")
-    self.thumb_image ||= Placeholder.image_generator(height: "350", width: "200")
-  end
+  # Due to the carrierwave gem, the following is overwritten
+  # def set_defaults
+  #   self.main_image  ||= Placeholder.image_generator(height: "600", width: "400")
+  #   self.thumb_image ||= Placeholder.image_generator(height: "350", width: "200")
+  # end
 
 end
 
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,10 +1,11 @@
 class Skill < ApplicationRecord
   validates_presence_of :title, :percent_utilized
-  after_initialize :set_defaults
-  # Call Placeholder module from concerns folder
-  include Placeholder
+  # We are not using the following...yet at least
+  # after_initialize :set_defaults
+  # # Call Placeholder module from concerns folder
+  # include Placeholder
 
-  def set_defaults
-    self.badge ||= Placeholder.image_generator(height: "250", width: "250")
-  end
+  # def set_defaults
+  #   self.badge ||= Placeholder.image_generator(height: "250", width: "250")
+  # end
 end
